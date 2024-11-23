1741f23897738e15d9506d6a054fb8e286af0873
RaphaelNY <1278173058@qq.com>
2024-11-23 20:55:13 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
     let regex = "a(bc)*";
-    let nfa = task2_1::build_nfa_from_regex(regex);
+    let nfa = task2::build_nfa_from_regex(regex);
     nfa.print_nfa();
 
     let dot_output = nfa.to_dot();
     // 假设你已经有函数来写入文件
-    task2_1::write_to_file("nfa.dot", &dot_output).expect("TODO: panic message");
+    task2::write_to_file("nfa.dot", &dot_output).expect("TODO: panic message");
 
     let mut dfa = nfa.to_dfa();
     let dot_output = dfa.to_dot();
-    task2_1::write_to_file("dfa.dot", &dot_output).unwrap();
+    task2::write_to_file("dfa.dot", &dot_output).unwrap();
 
     dfa.minimize();
     let dot_output = dfa.to_dot();
-    task2_1::write_to_file("minimized_dfa.dot", &dot_output).unwrap();
+    task2::write_to_file("minimized_dfa.dot", &dot_output).unwrap();
 }
