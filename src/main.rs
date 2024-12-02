3889a484783feb2e387f70e74a4fff294482837d
RaphaelNY <1278173058@qq.com>
2024-12-02 23:07:07 +0800

    添加循环输入测试

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let regex = task2::input(); // "a(bc)*"
-    let nfa = task2::build_nfa_from_regex(&*regex);
-    nfa.print_nfa();
-    task2::dot(nfa);
+    loop{
+        println!("chosse task or quit\n0. quit\n1. task");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input: i32 = input.trim().parse().unwrap();
+        match input {
+            0 => return,
+            1 => {
+                let regex = task2::input(); // "a(bc)*"
+                let nfa = task2::build_nfa_from_regex(&*regex);
+                nfa.print_nfa();
+                task2::dot(nfa);
+            }
+
+            _ => {
+                println!("Invalid input");
+            }
+        }
+
+    }
 }
